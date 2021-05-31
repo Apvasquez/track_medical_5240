6fc56032b3da97942d1f4c90cc214a5dde0c1ca0
Alex Paul <alex_12paul@hotmail.es>
2021-05-30 21:46:26 -0500

    App_Flutter

---
+++ android/app/src/main/kotlin/com/espe5240track_medical/flutter_projects/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.espe5240track_medical.flutter_projects
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //widget principal
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = ThemeData(primarySwatch: Colors.green);
+    MyHomePage homePage = MyHomePage(titulo: 'Des.Apps Móviles ');
+
+    MaterialApp result = MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: themeData,
+      home: homePage,
+    );
+
+    return result;
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.titulo}) : super(key: key);
+  final String titulo;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.titulo),
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(label: "Uno", icon: Icon(Icons.access_time)),
+          BottomNavigationBarItem(icon: Icon(Icons.accessible), label: "Dos"),
+          BottomNavigationBarItem(icon: Icon(Icons.accessible), label: "Dos"),
+        ]),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Usted ha pulsado cuantas veces el botón:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Text('NRC: 5240', style: Theme.of(context).textTheme.caption),
+              Icon(Icons.confirmation_num_outlined)
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons
+                .plus_one)) // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
